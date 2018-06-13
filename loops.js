5f3b92e789e9dbe840c46ea8c05e56c42d67f094
James M <saintjames@MacBook-Pro-2.local>
2018-06-12 22:42:44 -0600

    Created loops.js file

---
+++ loops.js
@@ -0,0 +1,25 @@
+for( var i = 0; i <= 99; i++){
+   console.log(i);
+}
+
+for( var i = 99; i >= 0; i--){
+   console.log(i);
+}
+
+for ( i=0 ; i<98 ; i+=2 ) {
+    console.log(i); 
+}
+
+for ( i=0 ; i>98 ; i-=2 ) {
+    console.log(i); 
+}
+
+for( var i = 49; i <= 72; i++){
+   console.log(i);
+
+
+for( var i = 81; i <= 26; i--){
+   console.log(i);
+
+   for( var i = 3; i <= 90; i+=3){
+   console.log(i);
